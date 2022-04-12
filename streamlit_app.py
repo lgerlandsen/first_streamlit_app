fa7af2bec60212e7ddcb1a7ac87360149440931f
Leif Gunnar Erlandsen <leif@lako.no>
2022-04-12 11:06:39 +0200

    Update streamlit_app.py
    added meny items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.tex('Kale, Spinach & Roket Smoothie')
+streamlit.text('Hard-Boiled Fre-Range Egg')
