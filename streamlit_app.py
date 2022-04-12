59dfb93015eb5e508c5020b23f97a75bc6a9ed68
Leif Gunnar Erlandsen <leif@lako.no>
2022-04-12 11:07:47 +0200

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.tex('Kale, Spinach & Roket Smoothie')
+streamlit.text('Kale, Spinach & Roket Smoothie')
 streamlit.text('Hard-Boiled Fre-Range Egg')
